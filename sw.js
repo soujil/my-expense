6b57c89dd53340b35dba962a3a5c6df7fae51077
soujil <48029954+soujil@users.noreply.github.com>
2026-09-11 17:22:40 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+// Service worker intentionally disabled in this stable build.
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
